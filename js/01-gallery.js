a4a80fb890d0f25f30f06cc66b4f6f00065b4f51
Alina219o <149413662+Alina219o@users.noreply.github.com>
2023-12-15 13:08:35 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
-// Ваш код
+//const refs = {
+  galleryListEl: document.querySelector('ul.gallery'),
+  modalContainerEl: document.querySelector('div.lightbox'),
+  lightboxOverlayEl: document.querySelector('div.lightbox__overlay'),
+  modalImgEl: document.querySelector('img.lightbox__image'),
+  modalCloseBtnEl: document.querySelector('[data-action="close-lightbox"]'),
+};
 
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+refs.galleryListEl.addEventListener('click', onGalleryItemClick);
+refs.modalCloseBtnEl.addEventListener('click', closeModal);
+refs.lightboxOverlayEl.addEventListener('click', closeModal); // 7.
 console.log(galleryItems);
