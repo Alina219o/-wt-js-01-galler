bd22220e66c943fb96549908ef836cc5535563bd
Alina219o <149413662+Alina219o@users.noreply.github.com>
2023-12-15 13:57:09 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,10 +6,81 @@ import { galleryItems } from './gallery-items.js';
   modalImgEl: document.querySelector('img.lightbox__image'),
   modalCloseBtnEl: document.querySelector('[data-action="close-lightbox"]'),
 };
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
 
-const galleryMarkup = createGalleryMarkup(galleryItems);
-refs.galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
-refs.galleryListEl.addEventListener('click', onGalleryItemClick);
-refs.modalCloseBtnEl.addEventListener('click', closeModal);
-refs.lightboxOverlayEl.addEventListener('click', closeModal); // 7.
-console.log(galleryItems);
+// create modal
+// function onImgClick(e) {
+//   e.preventDefault();
+//   // if (e.target.nodeName !== "IMG") return;
+
+//   const isItemImage = e.target.classList.contains('gallery__image');
+//   if (!isItemImage) return;
+
+//   const currentImgUrl = e.target.dataset.source;
+
+//   const instance = basicLightbox.create(
+//     `
+// 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+//         `,
+//     {
+//       onShow: (instance) => {
+//         window.addEventListener('keydown', onEscKeyPress);
+//       },
+//       onClose: (instance) => {
+//         window.removeEventListener('keydown', onEscKeyPress);
+//       },
+//     }
+//   );
+//   instance.show();
+
+//   function onEscKeyPress(e) {
+//     const ESC_KEY_CODE = 'Escape';
+//     const isEscKey = e.code === ESC_KEY_CODE;
+//     if (!isEscKey) return;
+//     instance.close();
+//   }
+// }
+
+// ------- 2nd variant --------------------------------------------------------------------------------
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
+}
